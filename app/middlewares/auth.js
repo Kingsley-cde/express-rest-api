e97a306f05fa1af0707059bb2b0b7728e2756dbc
kenechukwu ejeagwu <kinggleyejeagwu@gmail.com>
2024-03-23 14:13:32 +0100

    auth updated

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -1,16 +1,39 @@
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 dotenv.config();
-exports.auth = () => (req, res, next) => {
+
+exports.auth = (req, res, next) => {
   try {
+    console.log("authorization middleware");
     const authorization = req.headers.authorization;
-    if (!authorization) throw new Error("Invalid Authorization");
-    if (!authorization.startswith("Bearer "))
-      throw new Error("Invalid Authorization");
+    if (!authorization || !authorization.startsWith("Bearer")) {
+      return res.status(401).json({ msg: "Invalid Authorization" });
+    }
     const token = authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ msg: "No token, authorization denied" });
+    }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.id;
     next();
-  } catch (err) {
-    res.status(400).json({ message: err.message });
+  } catch (error) {
+    res.status(401).json({ msg: "Token is not valid" });
   }
 };
+
+// const dotenv = require("dotenv");
+// dotenv.config();
+// exports.auth = () => (req, res, next) => {
+//   try {
+//     const authorization = req.headers.authorization;
+//     if (!authorization) throw new Error("Invalid Authorization");
+//     if (!authorization.startswith("Bearer "))
+//       throw new Error("Invalid Authorization");
+//     const token = authorization.split(" ")[1];
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.userId = decoded.id;
+//     next();
+//   } catch (err) {
+//     res.status(400).json({ message: err.message });
+//   }
+// };
